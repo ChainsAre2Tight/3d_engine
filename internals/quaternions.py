be816a5426098d5848a3dd3efac4695d0cd5b0cd
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2023-11-03 15:42:50 +0300

    initial_commit

---
+++ internals/quaternions.py
@@ -0,0 +1,125 @@
+import math
+
+
+class Vector:
+    x: float = 0
+    y: float = 0
+    z: float = 0
+
+    def __init__(self, x, y, z):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def __str__(self):
+        return f'({self.x}, {self.y}, {self.z})'
+
+    @property
+    def length(self) -> float:
+        return (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+
+    def __add__(self, other):
+        if type(other) != Vector:
+            raise TypeError
+        return Vector(
+            x=self.x + other.x,
+            y=self.y + other.y,
+            z=self.z + other.z
+        )
+
+    def __mul__(self, other):
+        if type(other) == Vector:
+            raise NotImplementedError
+        if type(other) == int or type(other) == float:
+            raise NotImplementedError
+        else:
+            raise TypeError
+
+    def normalize(self):
+        length = self.length
+        return Vector(
+            x=round(self.x / length, 4),
+            y=round(self.y / length, 4),
+            z=round(self.z / length, 4)
+        )
+
+
+class Quaternion(Vector):
+    w: float = 0
+
+    def __init__(self, w, x, y, z):
+        super().__init__(x, y, z)
+        self.w = float(w)
+
+    @staticmethod
+    def from_euler(rotation_angle: float | int, rotate_axis: Vector | tuple):
+        if type(rotate_axis) == tuple:
+            rotate_axis = Vector(
+                x=rotate_axis[0],
+                y=rotate_axis[1],
+                z=rotate_axis[2]
+            )
+
+        rotate_axis = rotate_axis.normalize()
+
+        return Quaternion(
+            w=round(math.cos(rotation_angle / 2), 4),
+            x=round(rotate_axis.x * math.sin(rotation_angle / 2), 4),
+            y=round(rotate_axis.y * math.sin(rotation_angle / 2), 4),
+            z=round(rotate_axis.z * math.sin(rotation_angle / 2), 4)
+        )
+
+    def __str__(self):
+        return f'({self.w}, {self.x}, {self.y}, {self.z})'
+
+    def normalize(self):
+        length = self.length
+        return Quaternion(
+            w=self.w,
+            x=round(self.x / length, 4),
+            y=round(self.y / length, 4),
+            z=round(self.z / length, 4)
+        )
+
+    def invert(self):
+        return Quaternion(
+            w=self.w,
+            x=-self.x,
+            y=-self.y,
+            z=-self.z
+        )
+
+    def __add__(self, other):
+        raise TypeError
+
+    def __mul__(self, other):
+        if type(other) == Quaternion:
+            return Quaternion(
+                w=round(self.w * other.w - self.x * other.x - self.y * other.y - self.z * other.z, 4),
+                x=round(self.w * other.x + self.x * other.w + self.y * other.z - self.z * other.y, 4),
+                y=round(self.w * other.y - self.x * other.z + self.y * other.w + self.z * other.x, 4),
+                z=round(self.w * other.z + self.x * other.y - self.y * other.x + self.z * other.w, 4)
+            )
+        elif type(other) == Vector:
+            return Quaternion(
+                w=round(self.x * other.x - self.y * other.y - self.z * other.z, 4),
+                x=round(self.w * other.x + self.y * other.z - self.z * other.y, 4),
+                y=round(self.w * other.y - self.x * other.z + self.z * other.x, 4),
+                z=round(self.w * other.z + self.x * other.y - self.y * other.x, 4)
+            )
+        else:
+            raise TypeError
+
+
+def rotate_vector_by_quaternion(vector: Vector, quaternion: Quaternion) -> Vector:
+    resulting_vector = quaternion * vector * quaternion.invert()
+
+    return Vector(
+        x=resulting_vector.x,
+        y=resulting_vector.y,
+        z=resulting_vector.z,
+    )
+
+
+if __name__ == "__main__":
+    pass
---
+++ internals/tests/test_vectors.py
@@ -0,0 +1,95 @@
+import unittest
+from internals.quaternions import Vector, Quaternion, rotate_vector_by_quaternion
+from math import pi
+
+
+class TestVectors(unittest.TestCase):
+    def test_str(self):
+        vector = Vector(1.25, 3, -5.0)
+        self.assertEqual("(1.25, 3.0, -5.0)", str(vector))
+
+    def test_length(self):
+        vector = Vector(5, 6, 8)
+        self.assertEqual(125 ** 0.5, vector.length)
+
+    def test_add(self):
+        vector_A = Vector(5, 0, -3)
+        vector_B = Vector(-7, 8, 6)
+        vector_C = vector_A + vector_B
+        self.assertEqual("(-2.0, 8.0, 3.0)", str(vector_C))
+
+    def test_normalize(self):
+        vector_A = Vector(1, 1, 0)
+        vector_B = vector_A.normalize()
+        self.assertEqual("(0.7071, 0.7071, 0.0)", str(vector_B))
+
+    def test_mul_vector(self):
+        vector_A = Vector(5, 0, -3)
+        vector_B = Vector(-7, 8, 6)
+        with self.assertRaises(NotImplementedError):
+            vector_C = vector_A * vector_B
+
+    def test_mul_number(self):
+        vector_A = Vector(5, 0, -3)
+        with self.assertRaises(NotImplementedError):
+            vector_B = vector_A * 2
+
+    def test_fail_mul(self):
+        vector_A = Vector(1, 2, 3)
+        with self.assertRaises(TypeError):
+            vector_A * 'asd'
+
+
+class TestQuaternions(unittest.TestCase):
+
+    def test_str(self):
+        quat = Quaternion(1, 2, 3, 4)
+        self.assertEqual("(1.0, 2.0, 3.0, 4.0)", str(quat))
+
+    def test_from_euler(self):
+        quat = Quaternion.from_euler(pi / 2, (1, 0, 0))
+        self.assertEqual("(0.7071, 0.7071, 0.0, 0.0)", str(quat))
+
+    def test_add(self):
+        quaternion_A = Quaternion(1, 2, 3, 4)
+        quaternion_B = Quaternion(2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            quaternion_C = quaternion_A + quaternion_B
+
+    def test_fail_mul(self):
+        quaternion = Quaternion(1, 2, 3, 4)
+        with self.assertRaises(TypeError):
+            quaternion = quaternion * 2
+
+    def test_mul(self):
+        quaternion_A = Quaternion(0.7071, 0.7071, 0, 0)
+        quaternion_B = Quaternion(0.7071, 0, 0.7071, 0)
+        quaternion_C = quaternion_A * quaternion_B
+        self.assertEqual("(0.5, 0.5, 0.5, 0.5)", str(quaternion_C))
+
+    def test_mul_vector(self):
+        quaternion_A = Quaternion(0.7071, 0.7071, 0, 0)
+        vector_B = Vector(1, 0, 0)
+        res = quaternion_A * vector_B
+        self.assertEqual("(0.7071, 0.7071, 0.0, 0.0)", str(res))
+
+
+class TestVectorRotation(unittest.TestCase):
+    def test_1(self):
+        vector = Vector(0, 0, 1)
+        angle = pi
+        axis = Vector(1, 0, 1)
+
+        resulting_vector = rotate_vector_by_quaternion(
+            vector=vector,
+            quaternion=Quaternion.from_euler(
+                rotation_angle=angle,
+                rotate_axis=axis
+            )
+        )
+
+        self.assertEqual("(1.0, 0.0, 0.0)", str(resulting_vector))
+
+
+if __name__ == '__main__':
+    unittest.main()
