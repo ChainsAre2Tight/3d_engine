77f2acd277bfecd8d119b58f7009bc1deb45b2ec
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2023-11-08 12:56:45 +0300

    now uses Object dataclass rather than dictionary for storing information about objects

--- internals/handlers.py
+++ internals/handlers.py
@@ -8,6 +8,10 @@ class _AbstractHandler(ABC):
     pass
 
 
+class NoNormalsException(Exception):
+    pass
+
+
 class FileHandler(_AbstractHandler):
     _file_path: str
     _file_name: str
@@ -27,6 +31,8 @@ class FileHandler(_AbstractHandler):
         current_object = "None"
         res = dict()
         line_number = 0
+        previous_indexes = 0
+        previous_normals = 0
 
         for line in lines:
             line_number += 1
@@ -43,58 +49,70 @@ class FileHandler(_AbstractHandler):
                     raise NameError(f"Two objects have the same name {data} in .OBJ file {self._file_name}")
                 else:
                     # TODO replace with dataclass
-                    res[data] = {"polygons": list(), "vertices": list(), "smooth_shading": 0, "normals": list()}
+                    res[data] = internals.objects.Object(data, color=internals.rgb.next_color())
+                    # res[data] = {"polygons": list(), "vertices": list(), "smooth_shading": 0, "normals": list()}
             elif prefix == "v":
                 v1, v2, v3 = list(map(float, data.split()))
-                res[current_object]["vertices"].append(
+                # res[current_object]["vertices"].append(
+                #     internals.objects.Vertex(x=v1, y=v2, z=v3)
+                # )
+                res[current_object].vertices.append(
                     internals.objects.Vertex(x=v1, y=v2, z=v3)
                 )
+                previous_indexes += 1
             elif prefix == "s":
-                res[current_object]["smooth_shading"] = int(data)
+                # res[current_object]["smooth_shading"] = int(data)
+                res[current_object].smooth_shading = int(data)
             elif prefix == "vn":
-                res[current_object]["normals"].append(internals.vectors.Vector(*list(map(float, data.split()))))
+                # res[current_object]["normals"].append(internals.vectors.Vector(*list(map(float, data.split()))))
+                res[current_object].normals.append(internals.vectors.Vector(*list(map(float, data.split()))))
+                previous_normals += 1
             elif prefix == "f":
                 indexes = data.split()
 
                 for i in range(len(indexes)):
                     smol_data = indexes[i].split("/")
                     if len(smol_data) == 1:
-                        indexes[i] = smol_data[0]
+                        indexes[i] = int(smol_data[0])
+                        normal = None
+                        material = None
                     else:
-                        index = int(smol_data[0]) - 1
+                        index = int(smol_data[0]) - 1 - previous_indexes
                         material = smol_data[1]
-                        normal = int(smol_data[2]) - 1
-                        indexes[i] = index
-                polygon_normal = res[current_object]["normals"][normal]
-
+                        normal = int(smol_data[2]) - 1 - previous_normals
+                        indexes[i] = int(index)
+                # polygon_normal = res[current_object]["normals"][normal]
+                try:
+                    polygon_normal = res[current_object].normals[normal]
+                except TypeError:
+                    raise NoNormalsException("Normals are missing")
                 if len(indexes) == 3:
                     # print(indexes)
-                    res[current_object]["polygons"].append(
+                    res[current_object].polygons.append(
                         internals.objects.Polygon(
-                            first=res[current_object]["vertices"][indexes[0]],
-                            second=res[current_object]["vertices"][indexes[1]],
-                            third=res[current_object]["vertices"][indexes[2]],
-                            color=internals.rgb.light_gray_color(),
+                            first=res[current_object].vertices[indexes[0]],
+                            second=res[current_object].vertices[indexes[1]],
+                            third=res[current_object].vertices[indexes[2]],
+                            color=res[current_object].color,
                             normal=polygon_normal
                         )
                     )
                 elif len(indexes) == 4:
-                    quad = internals.objects.Quad(
-                        first=res[current_object]["vertices"][indexes[0]],
-                        second=res[current_object]["vertices"][indexes[1]],
-                        third=res[current_object]["vertices"][indexes[2]],
-                        fourth=res[current_object]["vertices"][indexes[3]],
-                        color=internals.rgb.light_gray_color(),
-                        normal=polygon_normal,
-                    )
-                    t1, t2 = quad.get_polygons()
-                    t1.color = internals.rgb.light_gray_color()
-                    t2.color = internals.rgb.light_gray_color()
-
-                    res[current_object]["polygons"].append(t1)
-                    res[current_object]["polygons"].append(t2)
+                        quad = internals.objects.Quad(
+                            first=res[current_object].vertices[indexes[0]],
+                            second=res[current_object].vertices[indexes[1]],
+                            third=res[current_object].vertices[indexes[2]],
+                            fourth=res[current_object].vertices[indexes[3]],
+                            color=res[current_object].color,
+                            normal=polygon_normal,
+                        )
+                        t1, t2 = quad.get_polygons()
+                        res[current_object].polygons.append(t1)
+                        res[current_object].polygons.append(t2)
                 else:
-                    raise NotImplementedError(f"Can only handle triangles and quads but got {len[indexes]}-gon")
+                    raise NotImplementedError(f"Can only handle triangles and quads but got {len(indexes)}-gon")
+
+
             else:
                 raise KeyError(
                     f'Got unexpected prefix "{prefix}" while reading {self._file_name} at line {line_number}')
@@ -137,7 +155,7 @@ class DataHandler(_AbstractHandler):
         self._objects = reader.interpret_file()
 
         for obj in self._objects.keys():
-            self._polygons.extend(self._objects[obj]["polygons"])
+            self._polygons.extend(self._objects[obj].polygons)
 
     def get_lines(self) -> list[internals.objects.Line]:
         return self._lines
